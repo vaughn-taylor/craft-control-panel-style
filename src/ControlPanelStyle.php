c2b7e73c2b29a983418dbc6042056d2b560e2af2
Vaughn D. Taylor <vaughn.taylor@gmail.com>
2024-06-13 12:44:41 -0500

    changed the schema number

--- src/ControlPanelStyle.php
+++ src/ControlPanelStyle.php
@@ -24,7 +24,7 @@ use yii\base\Event;
 class ControlPanelStyle extends Plugin
 {
     /** @var string */
-    public string $schemaVersion = '1.0.0';
+    public string $schemaVersion = '1.0.1';
 
     /** @var ControlPanelStyle */
     public static ControlPanelStyle $plugin;
